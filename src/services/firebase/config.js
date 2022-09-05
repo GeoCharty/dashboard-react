99cacb1c6b22f7fa45b2d5346fd4f523eac7c7ec
josue <josuealexandercs15@gmail.com>
2022-09-05 01:17:19 -0500

    Fix deploy files

---
+++ src/services/firebase/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  "type": "service_account",
+  "projectId": "geocharty-a6dff",
+  "apiKey": process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+  "privateKey": process.env.REACT_APP_FIREBASE_CONFIG_PRIVATE_KEY,
+  "authDomain": "firebase-adminsdk-v5tgm@geocharty-a6dff.iam.gserviceaccount.com",
+  "clientId": process.env.REACT_APP_FIREBASE_CONFIG_CLIENT_ID,
+  "authUri": "https://accounts.google.com/o/oauth2/auth",
+  "tokenUri": "https://oauth2.googleapis.com/token",
+}
